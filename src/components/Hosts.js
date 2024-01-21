ede65f16cdab8c41e4b00bbe0e0af8b934f8f274
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-01-21 09:14:21 +0100

    Add files via upload

---
+++ src/components/Hosts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserProfileCard from './UserProfileCard'
+
+function Hosts() {
+  return (
+    <div>
+      <UserProfileCard />
+    </div>
+  )
+}
+
+export default Hosts
