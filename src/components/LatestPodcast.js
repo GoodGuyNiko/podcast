ee3be9fc94c989019a8dc627e7587bb979e2a42c
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-05 15:57:05 +0100

    Add files via upload

--- src/components/LatestPodcast.js
+++ src/components/LatestPodcast.js
@@ -4,6 +4,7 @@ function LatestPodcast() {
   return (
     <div>
       <h2>Aktuelle Folge</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/0AXconl1jSE?si=iK6MWhM7NPDDttrJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   )
 }
