85e96071b9267aeae756391230cceaf0e1d0c511
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-05 16:04:47 +0100

    Add files via upload

--- src/components/LatestPodcast.js
+++ src/components/LatestPodcast.js
@@ -4,9 +4,15 @@ function LatestPodcast() {
   return (
     <div>
       <h2>Aktuelle Folge</h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/0AXconl1jSE?si=iK6MWhM7NPDDttrJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <div className='videoWrapper'>
+        <iframe width="560" height="315" 
+        src="https://www.youtube.com/embed/0AXconl1jSE?si=iK6MWhM7NPDDttrJ" 
+        title="YouTube video player" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; 
+        encrypted-media; gyroscope; picture-in-picture; web-share" 
+        allowfullscreen></iframe>
+      </div>
     </div>
   )
 }
-
 export default LatestPodcast
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import React from 'react';
 import '../styles/Home.css';
 import HeroSection from '../components/HeroSection';
 import About from '../components/About'
+import LatestPodcast from '../components/LatestPodcast';
 
 function Home() {
   return (
     <>
       <HeroSection />
       <About />
+      <LatestPodcast />
       {/* <Landing />
       <Landing2 />
       <Spotify /> */}
