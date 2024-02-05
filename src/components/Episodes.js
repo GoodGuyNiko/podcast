6fc1c3f2928284a34bdd0a218e7c13fef4178680
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-05 18:46:47 +0100

    Add files via upload

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -8,13 +8,16 @@ import branditEpisode2 from '../assets/brandit_episode2.png'
 function Episodes() {
   return (
     <div className='episode'>
-    <h1>Episode 1</h1>
+    <h1>Podcast Episoden</h1>
     <p>In Episode 1 blah blah blah blah</p>
     <div className='episodeCard'>
       <EpisodesData
       image={branditEpisode1}
-      heading="Episode 1"
-      text= "In Episode 1..."
+      heading="Wie finde ich den richtigen Namen für mein Brand?"
+      text= "Ein gut gewählter Name ist mehr als eine Bezeichnung; 
+      er ist das repräsentative Aushängeschild der Marke.
+      Im Podcast erklären wir, worauf es bei der Namenswahl ankommt: 
+      Einzigartigkeit, Einfachheit und klare Verbindung zum Produkt."
       />
             <EpisodesData
       image={branditEpisode2}
