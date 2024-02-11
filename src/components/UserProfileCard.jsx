69711e7ac7cc9c0fe657d611c12e9a3c320630bf
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-11 20:23:13 +0100

    Add files via upload

--- src/components/UserProfileCard.jsx
+++ src/components/UserProfileCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import '../styles/UserProfile.css'
-import SvenjaIcon from '../assets/Svenja_Schweres_brandit.jpg'
-import NikoIcon from '../assets/Niko_Schweres_brandit.jpg'
+import SvenjaIcon from '../assets/Svenja_Schweres.jpg'
+import NikoIcon from '../assets/Niko_Schweres.jpg'
 
 const UserProfileCard = () => {
   return (
     <div className='upc'>
+        <div className='profileDown'>
+        <img src={NikoIcon} alt='Foto Niko Schweres brandit.' />
+        <div className='profileTitle'>Niko Schweres</div>
+        <div className='profileDescription'>
+        Bacon ipsum dolor amet pancetta prosciutto filet mignon, sausage pork belly 
+        spare ribs doner. Kevin alcatra tail tenderloin bresaola, sausage hamburger 
+        ribeye tongue salami andouille short ribs turducken flank ham hock. 
+        Cupim bacon spare ribs andouille, doner kevin landjaeger.
+        </div>
+        <div className='profileButton'><a href='mailto:niko.schweres@greymountains.de'>Kontakt</a></div>
+      </div>
       <div className='gradiant'></div>
       <div className='profileDown'>
         <img src={SvenjaIcon} alt='Foto Svenja Schweres brandit.' />
@@ -19,17 +30,6 @@ const UserProfileCard = () => {
         <div className='profileButton'><a href='mailto:svenja.kircher@greymountains.de'>Kontakt</a></div>
       </div>
       <div className='gradiant'></div>
-      <div className='profileDown'>
-        <img src={NikoIcon} alt='Foto Niko Schweres brandit.' />
-        <div className='profileTitle'>Niko Schweres</div>
-        <div className='profileDescription'>
-        Bacon ipsum dolor amet pancetta prosciutto filet mignon, sausage pork belly 
-        spare ribs doner. Kevin alcatra tail tenderloin bresaola, sausage hamburger 
-        ribeye tongue salami andouille short ribs turducken flank ham hock. 
-        Cupim bacon spare ribs andouille, doner kevin landjaeger.
-        </div>
-        <div className='profileButton'><a href='mailto:niko.schweres@greymountains.de'>Kontakt</a></div>
-      </div>
     </div>
     
   )
