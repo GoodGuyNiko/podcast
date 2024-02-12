1e1d529e28de66b9e027a46b97e44ce2b461b41d
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-12 20:28:18 +0100

    Add files via upload

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -8,14 +8,16 @@ import Svenja from '../assets/Svenja_Schweres.jpg'
 function UserProfile() {
   return (
     <div className='user'>
-    <h1>brandit. Podcast</h1>
-    <p>Übersicht aller unserer Folgen</p>
+    <h1>Die Podcast Hosts</h1>
+    <p>Wer steckt hinter brandit. dem Branding Podcast?</p>
     <div className='userCard'>
       <UserProfileCard
       image={Niko}
       heading="Niko Schweres"
       text= "Ein gut gewählter Name ist mehr als eine Bezeichnung; er ist das repräsentative Aushängeschild der Marke. Im Podcast erklären wir, worauf es bei der Namenswahl ankommt: Einzigartigkeit, Einfachheit und klare Verbindung zum Produkt."
-      button= <a href='/branditfolge1'>Mehr</a>
+      // button= <a href='/branditfolge1'>Mehr</a>
+      button= <a href='https://www.instagram.com/brandit.podcast/' target="_blank">
+      <i className='fa-brands fa-instagram'></i></a>
       />
       <UserProfileCard
       image={Svenja}
