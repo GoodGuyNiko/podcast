287a5f9fca5acafe9d66a6f8ec87a334ba54e5c0
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-13 17:43:59 +0100

    Add files via upload

--- src/Folgen/Folge1.js
+++ src/Folgen/Folge1.js
@@ -9,7 +9,7 @@ function Folge1() {
     <img src={branditEpisode1} /></div>
     <h1 className='folgenHead'>Wie finde ich den richtigen Namen für mein Brand?</h1>
     <p className='subHead'>brandit. Folge 1</p>
-    <p>Marken- und Produktnamen sind nicht nur Wörter, sondern entscheidende Identitätsmarker im Branding. In unserer ersten Episode tauchen wir tief in dieses Thema ein, teilen eigene Fehler und wertvolle Erkenntnisse. Vor etwa 6 Jahren beispielsweise starteten wir eine vegane Kochshow namens "Team Ninja" auf YouTube. Ein Name, der aus der Fusion unserer Namen Svenja und Niko entstand. Leider existierte bereits eine Fortnite-Fangruppe mit dem gleichen Namen. Uppsi... Heute würden wir anders vorgehen bei der Namensfindung. In der ersten brandit. -Folge geben wir daher nicht nur unsere Tipps zur Namensfindung, sondern erklären euch auch, wie ihr unsere Fehler vermeiden könnt und euer Unternehmen mit einem einprägsamen Namen stärkt.
+    <p className='subText'>Marken- und Produktnamen sind nicht nur Wörter, sondern entscheidende Identitätsmarker im Branding. In unserer ersten Episode tauchen wir tief in dieses Thema ein, teilen eigene Fehler und wertvolle Erkenntnisse. Vor etwa 6 Jahren beispielsweise starteten wir eine vegane Kochshow namens "Team Ninja" auf YouTube. Ein Name, der aus der Fusion unserer Namen Svenja und Niko entstand. Leider existierte bereits eine Fortnite-Fangruppe mit dem gleichen Namen. Uppsi... Heute würden wir anders vorgehen bei der Namensfindung. In der ersten brandit. -Folge geben wir daher nicht nur unsere Tipps zur Namensfindung, sondern erklären euch auch, wie ihr unsere Fehler vermeiden könnt und euer Unternehmen mit einem einprägsamen Namen stärkt.
       Ein gut gewählter Name ist viel mehr als eine Bezeichnung; er ist ein Aushängeschild eurer Marke. Bei der Namensfindung ist daher sowohl Kreativität gefragt als auch klare und logische Überlegungen. Wir erläutern, worauf es ankommt, und teilen Tipps, um Stolpersteine zu umgehen. Einzigartigkeit ist im Branding beispielsweise entscheidend – der Name sollte nicht nur zum Unternehmen passen, sondern auch sicherstellen, dass keine Verwechslungen auftreten. Einfachheit ist ebenso wichtig; ein leicht aussprechbarer Name erleichtert Mundpropaganda und digitale Auffindbarkeit.
       Unsere Mission ist es, euch bei der Umsetzung eurer Ziele zu unterstützen und dafür unser Expertenwissen in Branding und Marketing zu teilen. Wir denken an alles, auch an oft übersehene Aspekte bei der Namensgebung von international vermarkteten Produkten.
       Ein Beispiel, worüber wir in der ersten Folge diskutieren, ist der Chevrolet Nova, dessen Name sich auf dem spanischen Markt als unglücklich erwies. "No va" bedeutet auf Spanisch "geht nicht", was offensichtlich nicht die beste Botschaft für ein Auto ist. Unser Rat: Achtet darauf, dass der Name keine negativen Assoziationen in anderen Sprachen hervorruft.
@@ -17,16 +17,17 @@ function Folge1() {
       Die erste Folge von brandit. ist nicht nur informativ, sondern auch interaktiv. Niko wird vor die Herausforderung gestellt, Markennamen wie "Kodak" und "IBM" in Kategorien einzuordnen. Wir diskutieren beschreibende Namen, erfundene Namen und solche, die nach Gründern benannt sind.
       Abschließend lässt sich sagen: Die Wahl eines Namens ist nicht nur kreativ, sondern auch ein strategischer Schachzug, der eure Marke formt. Es gibt keine einheitliche Lösung, aber Leitlinien, denen zu folgen sich lohnt. 
     </p>
-    <a href='https://open.spotify.com/show/3JYc6CejghMVvFXCrzZne8?si=1531890553d9411c' target="_blank">
+    <div className='folgenLinks'>
+    <a href='https://open.spotify.com/episode/3gfNQpeHbH2f3TTMAWEpIv?si=42edb3df6a054665' target="_blank">
       <i className='fa-brands fa-spotify'></i>
     </a>
-    <a href='https://open.spotify.com/show/3JYc6CejghMVvFXCrzZne8?si=1531890553d9411c' target="_blank">
+    <a href='https://podcasts.apple.com/de/podcast/brandit/id1724738282?i=1000640900479' target="_blank">
       <i class="fab fa-itunes"></i>
     </a>
-    <a href='https://open.spotify.com/show/3JYc6CejghMVvFXCrzZne8?si=1531890553d9411c' target="_blank">
-      <i className='fa-brands fa-spotify'></i>
+    <a href='https://youtu.be/MA9swnI6INg' target="_blank">
+      <i className='fa-brands fa-youtube'></i>
     </a>
-      
+    </div>  
   </div>
   )
 }
