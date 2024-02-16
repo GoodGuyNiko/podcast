4cfaf0568406ce92db0cb3f865ca1850a4dbfd70
g00dguy87 <135546206+g00dguy87@users.noreply.github.com>
2024-02-16 11:48:04 +0100

    Add files via upload

--- src/components/UeberUns.js
+++ src/components/UeberUns.js
@@ -5,7 +5,7 @@ function UeberUns() {
   return (
     <div className='ueberContainer'>
       <h1>Was ist brandit.?</h1>
-      <div>
+      <div className='aboutContainer'>
             <p> Es gibt einen Grund, warum manche Marken im Gedächtnis bleiben, während andere in der Masse untergehen. Und dieser Grund lautet Branding. Branding ist weit mehr als nur ein Logo oder ein schickes Design – es ist die Seele einer Marke, die Geschichte, die sie erzählt, und die Verbindung, die sie zu ihrer Zielgruppe aufbaut
                 <br />
                 <br />
@@ -22,6 +22,32 @@ function UeberUns() {
                 <br />
                 <br />
                 Lasst euch inspirieren von unseren Geschichten, tiefgehenden Analysen und praxiserprobten Ratschlägen. Willkommen bei brandit. - dem Podcast, der Branding lebendig macht.
+            </p>
+       </div>
+       <div className='aboutQuestions'>
+            <p>
+                <strong>Wer steckt hinter <em>brandit.</em>?</strong>
+                <br />
+                <br />
+                Svenja Schweres und Niko Schweres, Podcast-Hosts, Ehepaar und Geschäftspartner, sind die kreativen Köpfe und Gründer von Grey Mountains Media und <em>brandit.</em> – dem Brandingpodcast. Svenja ist eine erfahrene Videoproduzentin, während Niko seit knapp 20 Jahren als Kameramann tätig ist. Die beiden kennen sich in und auswendig und ergänzen sich daher perfekt als Team.
+                <br />
+                <br />
+                <strong>Die Vision hinter <em>brandit.</em></strong>
+                <br />
+                <br />
+                Die Gründung von <em>brandit.</em> entstand aus dem Wunsch heraus, unser Wissen und unsere Erfahrungen im Bereich Branding mit anderen zu teilen. Unsere Vision ist es, gemeinsam unseren Zuhörern und Zuschauern zu helfen, ihre Branding-Ziele zu erreichen. Dabei geht es darum, Gatekeeping zu überwinden und mit allen Menschen, die Interesse am Thema Branding haben, das Wissen zu teilen, über das wir verfügen.
+                <br />
+                <br />
+                <strong>Das Ziel von <em>brandit.</em></strong>
+                <br />
+                <br />
+                Das Ziel von <em>brandit.</em> ist es, unseren Zuhörern und Zuschauern praktische Tipps & Tricks, kostengünstige Branding-Strategien, theoretisches Wissen über Branding sowie effektive Strategien zu vermitteln, um sich von der Masse abzuheben. <em>brandit.</em> - der Branding-Podcast dient als Infotainment und soll den Zuhörern einen Mehrwert bieten, indem er sie inspiriert, informiert und befähigt, ihre eigenen Branding-Ziele zu erreichen. Wir möchten unseren Hörern nicht nur theoretisches Wissen vermitteln, sondern auch konkrete Handlungsempfehlungen geben, die ihnen dabei helfen, ihre Marke erfolgreich zu gestalten.
+                <br />
+                <br />
+                <strong>Welche Themen behandelt <em>brandit.</em>?</strong>
+                <br />
+                <br />
+                <em>brandit.</em> behandelt eine Vielzahl von Themen rund um Branding, die darauf abzielen, unseren Zuhörern ein umfassendes Verständnis und praktische Anleitungen zu bieten. Dazu gehören beispielsweise die Farbpsychologie im Branding, die Analyse erfolgreicher Branding-Strategien anhand von Fallbeispielen. Wir zeigen auf, wie Unternehmen durch die Nutzung von Videopodcasts ihre Markenbekanntheit steigern und eine engagierte Community aufbauen können. Zudem geben wir praktische Tipps und Best Practices für Brands, die eine Neupositionierung ihrer Marke in Betracht ziehen und erklären grundlegende Fragen wie "Was ist eigentlich Branding?" und "Wie wählt man den richtigen Brandnamen?"
             </p>
        </div>
     </div>
